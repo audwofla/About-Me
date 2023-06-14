8e3c7619138058c8566d5dc8ac8ab5751b24ee1f
audwofla <112502742+audwofla@users.noreply.github.com>
2023-06-13 18:16:10 -0700

    Delete unit-2-ps-1 directory

--- unit-2-ps-1/problem2.js
+++
@@ -1,8 +0,0 @@
-let fTemp = prompt("Enter temperature in Fahrenheit");
-let cTemp = ((fTemp - 32) * 5 / 9);
-alert(cTemp + " degrees Celsius");
-
-
-cTemp = prompt("Enter temperature in celsius");
-fTemp = ((cTemp * 9 / 5) + 32);
-alert(fTemp + " degrees Fahrenheit");
--- unit-2-ps-1/problem3.js
+++
@@ -1,8 +0,0 @@
-let num1 = prompt("First number");
-let num2 = prompt("Second number");
-
-if (num1 < num2) {
-    alert(num1 + " " + num2);
-} else {
-    alert(num2 + " " + num1);
-}
