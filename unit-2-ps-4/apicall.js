f19278ddff4789eb643d3568bb97ed1d58c37aae
audwofla <112502742+audwofla@users.noreply.github.com>
2023-07-05 16:05:42 -0700

    Add files via upload

--- unit-2-ps-4/apicall.js
+++ unit-2-ps-4/apicall.js
@@ -6,7 +6,6 @@ function callAPI() {
     fetch('https://randomuser.me/api/')
       .then(response => response.json())
       .then(data => {
-        console.log(data.results);
         let name = document.createElement('div');
         name.textContent = 'Name: ' + data.results[0].name.first + ' ' + data.results[0].name.last;
 
@@ -30,15 +29,34 @@ function callAPI() {
         let dob = document.createElement('div');
         dob.textContent = 'Date of Birth: ' + data.results[0].dob.date;
             
-        let game = document.querySelector('.rando');
-        game.appendChild(name);
-        game.appendChild(loc);
-        game.appendChild(email);
-        game.appendChild(cell);
-        game.appendChild(user);
-        game.appendChild(dob);
+        let rando = document.querySelector('.rando');
+        rando.appendChild(name);
+        rando.appendChild(loc);
+        rando.appendChild(email);
+        rando.appendChild(cell);
+        rando.appendChild(user);
+        rando.appendChild(dob);
 
-        
+        $.ajax({
+          url: 'https://randomuser.me/api/',
+          method: 'GET',
+          dataType: 'json',
+          success: function(data) {       
+            let name = $('<div>').text('Name: ' + data.results[0].name.first + ' ' + data.results[0].name.last);
+            let loc = $('<div>').text('Location: ' + data.results[0].location.street.number + ' ' 
+                                                   + data.results[0].location.street.name + ', ' 
+                                                   + data.results[0].location.city + ', ' 
+                                                   + data.results[0].location.state + ', ' 
+                                                   + data.results[0].location.postcode + ', ' 
+                                                   + data.results[0].location.country);
+            let email = $('<div>').text('Email: ' + data.results[0].email);
+            let cell = $('<div>').text('Cellphone: ' + data.results[0].cell);
+            let user = $('<div>').text('Username: ' + data.results[0].login.username);
+            let dob = $('<div>').text('Date of Birth: ' + data.results[0].dob.date);
+            
+            $('.random').append(name, loc, email, cell, user, dob);
+          },
+        });
 
 
       })
